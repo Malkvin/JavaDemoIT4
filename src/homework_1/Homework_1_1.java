051b8fb7d355eaffb28f04b60c97227e7a061c45
Malkvin <119970233+Malkvin@users.noreply.github.com>
2025-04-14 14:54:21 +0300

    added homework_1-5

---
+++ src/homework_1/Homework_1_1.java
@@ -0,0 +1,15 @@
+package homework_1;
+//Задача 1:
+//        Написать приложение, которое будет вычислять и выводить значение по
+//формуле: a=4*(b+c-1)/2;
+//b и c задаем в коде самостоятельно.
+public class Homework_1_1 {
+    public static void main(String[] args) {
+        int b = 4;
+        int c = 5;
+        int a = 4*(b+c-1)/2;
+        System.out.println(a);
+    }
+}
+
+
